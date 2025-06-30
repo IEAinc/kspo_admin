87739bbda439ca8739069e52c9c35e69793e41b3
jungeun0430 <je.lee@iea.co.kr>
2025-06-30 09:25:25 +0900

    :lipstick: 사이드바 주석처리

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -34,17 +34,25 @@ const Sidebar = () => {
         { title: 'FAQ 관리', path: '/ksponcoadministrator/scenarioManagement/faqManagement',state:{type:'FAQ'}  },
       ],
     },
+    // {
+    //   title: '만족도 관리',
+    //   path: '/ksponcoadministrator/menu1',
+    //   icon: {
+    //     default: Icon2,
+    //     active: Icon2Active,
+    //   },
+    //   subMenu: [
+    //     {title:'만족도 관리', path:'/ksponcoadministrator/satisfactionManagement/satisfactionManagement'},
+    //     {title:'사용자 통계', path:'/ksponcoadministrator/satisfactionManagement/userStatistics'}
+    //   ]
+    // },
     {
       title: '만족도 관리',
-      path: '/ksponcoadministrator/menu1',
+      path: '/ksponcoadministrator/satisfactionManagement/satisfactionManagement',
       icon: {
         default: Icon2,
         active: Icon2Active,
       },
-      subMenu: [
-        {title:'만족도 관리', path:'/ksponcoadministrator/satisfactionManagement/satisfactionManagement'},
-        {title:'사용자 통계', path:'/ksponcoadministrator/satisfactionManagement/userStatistics'}
-      ]
     },
     {
       title: '관리자 관리',
@@ -115,19 +123,27 @@ const Sidebar = () => {
               { title: 'FAQ 관리', path: '/ksponcoadministrator/scenarioManagement/faqManagement',state:{type:'FAQ'}  },
             ],
           },
+          // {
+          //   title: '만족도 관리',
+          //   path: '/ksponcoadministrator/menu1',
+          //   icon: {
+          //     default: Icon2,
+          //     active: Icon2Active,
+          //   },
+          //   subMenu: [
+          //     {title:'만족도 관리', path:'/ksponcoadministrator/satisfactionManagement/satisfactionManagement'},
+          //     {title:'사용자 통계', path:'/ksponcoadministrator/satisfactionManagement/userStatistics'}
+          //   ]
+          // },
+          //
           {
             title: '만족도 관리',
-            path: '/ksponcoadministrator/menu1',
+            path: '/ksponcoadministrator/satisfactionManagement/satisfactionManagement',
             icon: {
               default: Icon2,
               active: Icon2Active,
             },
-            subMenu: [
-              {title:'만족도 관리', path:'/ksponcoadministrator/satisfactionManagement/satisfactionManagement'},
-              {title:'사용자 통계', path:'/ksponcoadministrator/satisfactionManagement/userStatistics'}
-            ]
           },
-     
           {
             title: '이력 관리',
             path: '/ksponcoadministrator/menu1',
