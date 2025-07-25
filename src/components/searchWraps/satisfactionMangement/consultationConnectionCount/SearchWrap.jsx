d47f0cd9ef3058ee00d2c34368b06f3bb6824176
GwonBoemJuns <bj.gwon@iea.co.kr>
2025-07-25 10:09:47 +0900

    회사명 디폴트트

--- src/components/searchWraps/satisfactionMangement/consultationConnectionCount/SearchWrap.jsx
+++ src/components/searchWraps/satisfactionMangement/consultationConnectionCount/SearchWrap.jsx
@@ -32,8 +32,15 @@ const SearchWrap = ({onSearch}) => {
       const { company, id } = await fetchCommonData();
       let initCompany=company;
       let cookieCompany=Cookies.get("admincompany")
+      console.log(cookieCompany)
       if(cookieCompany!==undefined)initCompany=cookieCompany;
       if(cookieCompany==='null')initCompany=null;
+      if(initCompany===null){
+        setSelectedCenter({value:initCompany, label:"전체"})
+      }else{
+        setSelectedCenter({value:initCompany, label:initCompany})
+      }
+    
       // 날자 세팅
       let now= new Date();
       let before_month=new Date();
